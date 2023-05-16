a2d9fdaa64a866f09d422ff89403bb4bba194069
Radolina <radolina@Radolinas-MBP.home>
2023-05-16 12:17:31 +0200

    #9

---
+++ ArtDetective/ArtDetective/ArtDetectiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtDetectiveApp.swift
+//  ArtDetective
+//
+//  Created by Radolina on 15/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct ArtDetectiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArtDetective/ArtDetective/HomeView.swift
@@ -0,0 +1,55 @@
+//
+//  HomeView.swift
+//  ArtDetective
+//
+//  Created by Radolina on 15/05/2023.
+//
+
+import SwiftUI
+ 
+struct CreateBgStyle: ButtonStyle{
+    func makeBody(configuration: Self.Configuration)->some View{
+        configuration.label.frame(minWidth: 0, maxWidth: .infinity)
+            .padding()
+            .foregroundColor(.white)
+            .background(LinearGradient(colors: [Color("LogoPurple"), Color("LogoBlue")], startPoint: .leading, endPoint: .trailing))
+            .cornerRadius(15)
+            .padding(.horizontal, 15)
+    }
+}
+
+struct HomeView: View {
+    
+    var body: some View {
+        VStack{
+            Image("logo")
+        
+            
+            Button(action:{
+                
+            }){
+                HStack {
+                    Text("Start scaning")
+                        .font(.title)
+                        .bold()
+                    Image(systemName: "camera")
+                        .font(.title)
+                        .bold()
+                }
+                
+            }
+            .buttonStyle(CreateBgStyle())
+            
+        }
+        
+        
+    }
+    
+    
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
